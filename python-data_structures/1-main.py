b62ceb184a442425cc3361693041c518eb74fef5
Niber236 <tonmail@gmail.com>
2026-01-20 17:38:36 +0100

    All tasks done

--- python-data_structures/1-main.py
+++ python-data_structures/1-main.py
@@ -4,4 +4,3 @@ element_at = __import__('1-element_at').element_at
 my_list = [10, 20, 30, 40, 50]
 idx = 3
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
-## The code above is for testing the 'element_at' function from '1-element_at.py'.
