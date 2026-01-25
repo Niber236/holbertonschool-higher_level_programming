dfd93fdd45517c19794c135a7023191504c01485
Niber236 <tonmail@gmail.com>
2026-01-26 00:02:28 +0100

    Fix task 1 and add tasks 3 4

---
+++ python-test_driven_development/tests/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module for text indentation.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    flag = 0
+    for char in text:
+        if flag == 0:
+            if char == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if char == '?' or char == '.' or char == ':':
+                print(char)
+                print()
+                flag = 0
+            else:
+                print(char, end="")
