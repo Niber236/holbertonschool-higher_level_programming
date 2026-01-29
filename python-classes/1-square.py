cec554db096c6c1743d75f9e94d730581e3ba886
Niber236 <tonmail@gmail.com>
2026-01-29 14:38:12 +0100

    Defines a class  with a private instance attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe Square.
+"""
+
+
+class Square:
+    """
+    Cette classe définit un carré.
+    """
+
+    def __init__(self, size):
+        """
+        Initialise un nouveau carré.
+
+        Args:
+            size: La taille du carré.
+        """
+        self.__size = size
+     
