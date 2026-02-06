44e2641326f73bf84a4319b9a433adb24dc48e1c
Niber236 <tonmail@gmail.com>
2026-02-06 12:40:21 +0100

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Defines class BaseGeometry with public instance method area.
+"""
+
+
+class BaseGeometry:
+    """
+    A class representing base geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
