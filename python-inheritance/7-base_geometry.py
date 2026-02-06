679f04284457f508d22f59dadbb174b048378b64
Niber236 <tonmail@gmail.com>
2026-02-06 13:29:09 +0100

    Fix Task 7: Add doctest and verify validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,19 +19,8 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """
         Validates that a value is a positive integer.
-
-        Args:
-            name: The name of the value (string).
-            value: The value to validate (int).
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
         """
-        # 1. Vérification du Type (On veut un int pur)
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
-        # 2. Vérification de la Valeur (Strictement positive)
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
