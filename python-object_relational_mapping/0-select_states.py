4be1c60a5688d446b40a41d49e343ca7a37cb4ee
Niber236 <tonmail@gmail.com>
2026-03-06 23:42:52 +0100

    Fix Tasks 0 and 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa
-"""
-import sys
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    # On ouvre la porte de la base de données
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    # On crée un "curseur", c'est le bras mécanique qui va exécuter le SQL
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    # On balance la requête SQL pure
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # On récupère toutes les lignes et on les affiche une par une
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    # On nettoie derrière nous
     cur.close()
     db.close()
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
-import sys
+"""Script that lists all states starting with N from the database"""
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    # Connexion à la base
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    
-    # La requête filtrée avec BINARY pour la majuscule
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
-    
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-        
     cur.close()
     db.close()
