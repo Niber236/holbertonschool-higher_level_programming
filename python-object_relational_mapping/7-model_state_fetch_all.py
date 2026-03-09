e5f051d1e5eb51973f90ad7a721cea9d7bab50ae
Niber236 <tonmail@gmail.com>
2026-03-09 16:23:10 +0100

    Fix Task 7: Correct filename

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
-Module that contains the class definition of a State
-and an instance Base = declarative_base()
+Script that lists all State objects from the database hbtn_0e_6_usa
 """
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-
-Base = declarative_base()
-
-
-class State(Base):
-    """
-    Class State that inherits from Base.
-    Links to the MySQL table 'states'.
-    """
-    __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+        
+    session.close()
