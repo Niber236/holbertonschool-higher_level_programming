e2580313384b9d381f6d6cd066e07de9ac611ecd
Niber236 <tonmail@gmail.com>
2026-03-22 21:05:18 +0100

    correction

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
+const argCount = process.argv.length;
 
-const args = process.argv.length - 2;
-
-if (args === 0) {
+if (argCount === 2) {
   console.log('No argument');
-} else if (args === 1) {
+} else if (argCount === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
